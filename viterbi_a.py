be67cd617ae0251bf095e6b29981ea774355dfee
Starbuck7410 <starbuck7410@gmail.com>
2025-07-01 22:16:56 +0300

    fixed emission coefficients

--- viterbi_a.py
+++ viterbi_a.py
@@ -3,7 +3,9 @@ import numpy as np
 
 def get_note_vector(note, alpha):
     C = [1+alpha+alpha**3+alpha**7, 0, 0, 0, alpha**4, 0, 0, alpha**2+alpha**7, 0, 0, alpha**6, 0]
-    return C[note:] + C[:note]
+    if note > 11:
+        note -= 12
+    return C[12 - note:] + C[:12 - note]
 
 def get_major_chord(note, alpha):
     a = get_note_vector(note, alpha)
@@ -18,7 +20,7 @@ def get_minor_chord(note, alpha):
     return [x + y + z for x, y, z in zip(a, b, c)]
 
 
-def get_emission_coeff(chord, detection, alpha = 0.75):
+def get_emission_coeff(chord, detection, alpha = 0.5):
     chord_vec = []
     if chord < 12:
         chord_vec = get_major_chord(chord, alpha)
@@ -71,18 +73,21 @@ def get_initial_prob_array():
 
 def viterbi(detection_matrix, test = False):
     initial_prob_array = get_initial_prob_array()
-    transition_matrix = get_transition_matrix()
+    transition_matrix = get_transition_matrix(test)
     n = len(transition_matrix[0])
     m = len(detection_matrix)
+
     V = np.zeros((m, n))
-    backpointer = np.zeros((m, n))
+    backpointer = np.zeros((m, n), dtype=int)  # integer type here!
+
     for i in range(n):
         V[0][i] = initial_prob_array[i] * get_emission_coeff(i, detection_matrix[0])
-        backpointer[0][i] = None
+        backpointer[0][i] = -1  # No previous state for t=0
+
     for t in range(1, m):
         for i in range(n):
             max_prob = -1
-            best_prev = None
+            best_prev = -1
             for j in range(n):
                 prob = V[t-1][j] * transition_matrix[j][i] * get_emission_coeff(i, detection_matrix[t])
                 if prob > max_prob:
@@ -90,16 +95,18 @@ def viterbi(detection_matrix, test = False):
                     best_prev = j
             V[t][i] = max_prob
             backpointer[t][i] = best_prev
+
     max_final_prob = np.max(V[m-1])
     last_state = np.argmax(V[m-1])
-    best_path = np.array([last_state])
-    counter = 0
+
+    best_path = [last_state]
     for t in range(m-1, 0, -1):
-        print(best_path)
-        best_path = np.append(best_path, backpointer[t][best_path[counter]].astype(int))
-        counter += 1
-    best_path = np.flip(best_path)
-    return best_path, max_final_prob
+        last_state = backpointer[t][last_state]
+        best_path.append(last_state)
+    best_path.reverse()
+
+    return np.array(best_path), max_final_prob
+
 
 
 song = [
